8777513f21e0bc59ae3292a1e128927d3a0e6b20
syou0331 <86101556+syou0331@users.noreply.github.com>
2022-01-05 18:50:05 +0900

    商品一覧部を実装

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,5 +2,6 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
     @taxonomies = Spree::Taxon.roots
+    @products = @taxon.all_products.includes(master: [:default_price, :images])
   end
 end
