9cfe28b99e747f708d3c2c796fef1c480509b6ea
syou0331 <86101556+syou0331@users.noreply.github.com>
2022-01-07 18:30:17 +0900

    categoriesのcontrollerテストを実装

--- spec/requests/potepan/categories_spec.rb
+++ spec/requests/potepan/categories_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "Potepan::Categories", type: :request do
-  describe "GET /show" do
-    it "returns http success" do
-      get "/potepan/categories/show"
+RSpec.describe "Potepan::CategoriesController", type: :request do
+  describe "GET /potepan/categories/:id" do
+    let(:taxon) { create(:taxon) }
+
+    before do
+      get potepan_category_path(taxon.id)
+    end
+
+    example "HTTPのステータスコードが200であること" do
       expect(response).to have_http_status(:success)
+    end
+
+    example "@taxonとtaxonが一致すること" do
+      expect(controller.instance_variable_get("@taxon")).to eq taxon
     end
   end
 end
