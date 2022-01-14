eb80bc52314e8f9b1db7d63ff09c9fe12524887c
syou0331 <86101556+syou0331@users.noreply.github.com>
2022-01-14 18:01:07 +0900

    @related_productsを追加

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,5 +1,8 @@
 class Potepan::ProductsController < ApplicationController
+  DISPLAY_LIMIT = 4
+
   def show
     @product = Spree::Product.find(params[:id])
+    @related_products = @product.related_products(DISPLAY_LIMIT).includes(master: [:images, :default_price])
   end
 end
