7ea3c798c90fff9fcf2e2676ec4f9f7704287e31
syou0331 <86101556+syou0331@users.noreply.github.com>
2022-01-20 17:10:27 +0900

    product_decoratorのテストを実装

---
+++ spec/models/spree/product_decorator_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Spree::ProductDecorator, type: :model do
+  let(:taxon) { create(:taxon) }
+  let(:product) { create(:product, taxons: [taxon]) }
+  let!(:related_products) { create_list(:product, 5, taxons: [taxon]) }
+  let!(:unrelated_product) { create(:product, taxons: [create(:taxon)]) }
+  let(:duplicate_related_products) { related_products.push(related_products.first) }
+
+  it "関連商品が取得され、且つ関連しない商品は含まないこと" do
+    related_products.all? { |related_product| expect(product.related_products).to include related_product }
+    expect(product.related_products).not_to include unrelated_product
+  end
+
+  it "メイン商品が関連商品に含まないこと" do
+    expect(product.related_products).not_to include product
+  end
+
+  it "各関連商品は一意的であること" do
+    expect(product.related_products).to eq duplicate_related_products.uniq
+  end
+end
