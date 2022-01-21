52fb9afd69111f5ab1b8860198357f68f6fac26d
syou0331 <86101556+syou0331@users.noreply.github.com>
2022-01-21 09:13:25 +0900

    関連商品の最大表示数の変数名をMAX_DISPLAY_COUNTに変更

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,8 +1,8 @@
 class Potepan::ProductsController < ApplicationController
-  DISPLAY_LIMIT = 4 # 関連商品の最大表示数
+  MAX_DISPLAY_COUNT = 4 # 関連商品の最大表示数
 
   def show
     @product = Spree::Product.find(params[:id])
-    @related_products = @product.related_products.includes(master: [:images, :default_price]).limit(DISPLAY_LIMIT)
+    @related_products = @product.related_products.includes(master: [:images, :default_price]).limit(MAX_DISPLAY_COUNT)
   end
 end
